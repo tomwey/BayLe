841623e550021384393524db4471989d347fa3bf
tomwey <tomwey@163.com>
2015-11-20 17:32:00 +0800

    add tab stripper

---
+++ BayLe/Views/PagerTabStripper.h
@@ -0,0 +1,16 @@
+//
+//  PagerTabStripper.h
+//  BayLe
+//
+//  Created by tangwei1 on 15/11/20.
+//  Copyright © 2015年 tangwei1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PagerTabStripper : UIView
+
+
+
+@end
+
---
+++ BayLe/Views/PagerTabStripper.m
@@ -0,0 +1,13 @@
+//
+//  PagerTabStripper.m
+//  BayLe
+//
+//  Created by tangwei1 on 15/11/20.
+//  Copyright © 2015年 tangwei1. All rights reserved.
+//
+
+#import "PagerTabStripper.h"
+
+@implementation PagerTabStripper
+
+@end
